5aa7d442269ca3bd4adf751d2486945906494e51
GUSTAVO PORTELA DE LIMA <GUSTAVO_4657@uirapuru.br>
2025-09-03 10:46:03 -0300

    switch aninhado

--- src/logicaExercicios/SwitchAninhado.java
+++ src/logicaExercicios/SwitchAninhado.java
@@ -1,9 +1,35 @@
 package logicaExercicios;
 
+import java.util.Scanner;
+
 public class SwitchAninhado {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("MENU COM OPÇÔES");
+        System.out.println("ESCREVA A OPÇÂO ABAIXO:");
+        int resposta = sc.nextInt();
+        switch (resposta) {
+            case 1:
+                System.out.println("categoria selecionada : matematica:");
+                System.out.println("selecione a subcategoria abaixo: ");
+                int respostaSubCategoria = sc.nextInt();
+                switch (resposta) {
+                    case 1:
+                        System.out.println("calcular soma");
+                        break;
+
+                    case 2:
+                        System.out.println("calcular divisão");
+                        break;
+
+                }
+
+
+        }
 
 
     }
+
+
 }
+
